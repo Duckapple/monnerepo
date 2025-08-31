0ad345f3a5f5ac145768704329814784e9e30130
Simon Green Kristensen <git@simon-green.dev>
2025-08-31 12:39:48 +0200

    fix: lint stuff

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		experimental: {
+			remoteFunctions: true
+		}
+	},
 	extensions: ['.svelte', '.svx'],
 	compilerOptions: {
+		experimental: {
+			async: true
+		},
 		runes: true
 	}
 };
